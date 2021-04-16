e7225ed4442fd38dcd96866bc102877ac3e8e208
Chakrit Wichian <service@chakrit.net>
2021-04-16 14:30:59 +0700

    Add some log & signal handling niceties

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	go func() {
+		ch := make(chan os.Signal)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		<-ch
+		log.Fatalln("terminated")
+	}()
+
 	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("request from " + r.Host)
 		fmt.Fprintln(w, "Hello, World!")
 	}))
 }
